d226a862fc006e78d0d2f8682d1e00fc47533c47
Pedro Torales <pedroimanoltorales@gmail.com>
2022-09-30 00:36:45 -0300

    fdsf

--- ruby/lib/mockeable.rb
+++ ruby/lib/mockeable.rb
@@ -7,12 +7,12 @@ module Mockeable
   def self.restaurar
     @mocks ||= []
     @mocks.each do |metodo|
-      metodo.receiver.define_singleton_method(metodo.name, metodo)
+      metodo.owner.define_method(metodo.name, metodo)
     end
   end
 
   def mockear(nombre, &bloque)
-    Mockeable.guardar(method(nombre))
-    define_singleton_method(nombre, &bloque)
+    Mockeable.guardar(instance_method(nombre))
+    define_method(nombre, &bloque)
   end
 end
--- ruby/spec/mocks_spec.rb
+++ ruby/spec/mocks_spec.rb
@@ -1,9 +1,8 @@
 require_relative './helpers'
 
 class PersonaHome
-  include Singleton
-
   def todas_las_personas
+    ["jusepe","jusepa"]
     # Este método consume un servicio web que consulta una base de datos
   end
 
@@ -11,6 +10,7 @@ class PersonaHome
     self.todas_las_personas.select{|p| p.viejo?}
   end
 end
+
 
 describe "Mocks" do
   before do
@@ -27,14 +27,15 @@ describe "Mocks" do
     lean = Persona.new(22)
 
     # Mockeo el mensaje para no consumir el servicio y simplificar el test
-    PersonaHome.instance.mockear(:todas_las_personas) do
+    PersonaHome.mockear(:todas_las_personas) do
       [nico, axel, lean]
     end
 
-    viejos = PersonaHome.instance.personas_viejas
+    viejos = PersonaHome.new.personas_viejas
+
 
     expect(viejos).to match_array [nico, axel]
-  end
+     end
 
   it "Se desmockea aún mejor" do
     nico = Persona.new(30)
@@ -42,13 +43,17 @@ describe "Mocks" do
     lean = Persona.new(22)
 
     # Mockeo el mensaje para no consumir el servicio y simplificar el test
-    PersonaHome.instance.mockear(:todas_las_personas) do
+    PersonaHome.mockear(:todas_las_personas) do
       [nico, axel, lean]
     end
 
+    viejos = PersonaHome.new.personas_viejas
+
+    expect(viejos).to match_array [nico, axel]
+
     Mockeable.restaurar
 
-    viejos = PersonaHome.instance.personas_viejas
+    viejos = PersonaHome.new.personas_viejas
 
     expect(viejos).not_to match_array [nico, axel]
   end
