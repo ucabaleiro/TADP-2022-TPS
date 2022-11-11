ac49686ae99716571ef7f2f4ee2a4ad10daa71f4
Agustin Ranieri <aguseranieri@gmail.com>
2022-11-11 15:57:54 -0300

    Quito los unapply

--- scala/src/main/scala/calabozos/Puerta.scala
+++ scala/src/main/scala/calabozos/Puerta.scala
@@ -8,8 +8,8 @@ case class Puerta (obstaculos: List[Obstaculo], ubicacion: Ubicacion)
 trait Obstaculo extends (Grupo => Boolean) {
   def apply(grupo: Grupo): Boolean = grupo.heroesVivos.exists(puedeSerSuperadoPorHeroe(_, grupo.cofre))
 
-  def puedeSerSuperadoPorHeroe(heroe: Heroe, cofre: Cofre): Boolean = heroe match {
-    case Ladron(ladron) if ladron.tieneHabilidad(heroe.nivel, 20) => true
+  def puedeSerSuperadoPorHeroe(heroe: Heroe, cofre: Cofre): Boolean = heroe.trabajo match {
+    case ladron: Ladron if ladron.tieneHabilidad(heroe.nivel, 20) => true
     case _ => puedeSerSuperadoPorHeroeSegunObstaculo(heroe, cofre)
   }
 
@@ -17,23 +17,23 @@ trait Obstaculo extends (Grupo => Boolean) {
 }
 
 object Cerrada extends Obstaculo {
-  override def puedeSerSuperadoPorHeroeSegunObstaculo(heroe: Heroe, cofre: Cofre): Boolean = heroe match {
-    case Ladron(ladron) if ladron.tieneHabilidad(heroe.nivel, 10) || cofre.contains(Ganzua) => true
+  override def puedeSerSuperadoPorHeroeSegunObstaculo(heroe: Heroe, cofre: Cofre): Boolean = heroe.trabajo match {
+    case ladron: Ladron if ladron.tieneHabilidad(heroe.nivel, 10) || cofre.contains(Ganzua) => true
     case _ => cofre.contains(Llave)
   }
 }
 
 object Escondida extends Obstaculo {
-  override def puedeSerSuperadoPorHeroeSegunObstaculo(heroe: Heroe, cofre: Cofre): Boolean = heroe match {
-    case Mago(mago) => mago.sabeHechizo(heroe.nivel, Vislumbrar)
-    case Ladron(ladron) => ladron.tieneHabilidad(heroe.nivel, 6)
+  override def puedeSerSuperadoPorHeroeSegunObstaculo(heroe: Heroe, cofre: Cofre): Boolean = heroe.trabajo match {
+    case mago: Mago => mago.sabeHechizo(heroe.nivel, Vislumbrar)
+    case ladron: Ladron => ladron.tieneHabilidad(heroe.nivel, 6)
     case _ => false
   }
 }
 
 case class Encantada(hechizo: Hechizo) extends Obstaculo {
-  override def puedeSerSuperadoPorHeroeSegunObstaculo(heroe: Heroe, cofre: Cofre): Boolean = heroe match {
-    case Mago(mago) => mago.sabeHechizo(heroe.nivel, hechizo)
+  override def puedeSerSuperadoPorHeroeSegunObstaculo(heroe: Heroe, cofre: Cofre): Boolean = heroe.trabajo match {
+    case mago: Mago => mago.sabeHechizo(heroe.nivel, hechizo)
     case _ => false
   }
 }
--- scala/src/main/scala/calabozos/Trabajo.scala
+++ scala/src/main/scala/calabozos/Trabajo.scala
@@ -4,36 +4,12 @@ sealed trait Trabajo
 
 case class Guerrero() extends Trabajo
 
-object Guerrero {
-  def unapply(trabajo: Trabajo): Boolean = trabajo.isInstanceOf[Guerrero]
-  def unapply(heroe: Heroe): Option[Guerrero] = heroe.trabajo match {
-    case guerrero: Guerrero => Some(guerrero)
-    case _ => None
-  }
-}
-
 case class Ladron(habilidadBase: Int) extends Trabajo {
   def tieneHabilidad(nivel: Int, habilidad: Int): Boolean = (habilidadBase + nivel * 3) >= habilidad
 }
 
-object Ladron {
-  def unapply(trabajo: Trabajo): Boolean = trabajo.isInstanceOf[Ladron]
-  def unapply(heroe: Heroe): Option[Ladron] = heroe.trabajo match {
-    case ladron: Ladron => Some(ladron)
-    case _ => None
-  }
-}
-
 case class Mago(aprendizajes: List[Aprendizaje]) extends Trabajo {
   def sabeHechizo(nivel: Int, hechizo: Hechizo): Boolean = aprendizajes.exists(_(nivel, hechizo))
-}
-
-object Mago {
-  def unapply(trabajo: Trabajo): Boolean = trabajo.isInstanceOf[Mago]
-  def unapply(heroe: Heroe): Option[Mago] = heroe.trabajo match {
-    case mago: Mago => Some(mago)
-    case _ => None
-  }
 }
 
 case class Aprendizaje(hechizo: Hechizo, nivel: Int) extends ((Int, Hechizo) => Boolean) {
